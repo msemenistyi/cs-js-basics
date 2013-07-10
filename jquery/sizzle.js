478c6cdfb5a5ba45301f1d0d21cc94797704483d
Mykyta Semenistyi <nikeiwe@gmail.com>
2013-07-10 12:28:37 +0300

    initial commit

---
+++ jquery/sizzle.js
@@ -0,0 +1,19 @@
+$('div') //element selector
+
+$('.class') //class selectors
+$('span.class') //class selectors
+
+$('#id') //id selectors
+$('ul#id') //id selectors
+
+$('ul > li') //child selector
+$('ul span') //descendant selector
+$('ul').find('span'); //the same
+
+$("[data-main='5']"); // attr selector
+
+$("[name*='value']"); // attribute Contains Selector 
+$("[name~='value']"); // attribute Contains Word Selector 
+$("[name$='value']"); // attribute Ends With Selector 
+$("[name!='value']"); // attribute Not Equal Selector 
+$("[name^='value']"); // attribute Starts With Selector 
