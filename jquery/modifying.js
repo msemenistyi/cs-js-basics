a64ad364e82824761f38e6607841dbd64dbd8207
Mykyta Semenistyi <nikeiwe@gmail.com>
2013-07-10 14:39:20 +0300

    added modifying

---
+++ jquery/modifying.js
@@ -0,0 +1,12 @@
+var container = $('div');
+
+container.remove(); // removes all the matched items 
+
+container.detach(); // the same, but keeps jQuery-relative data
+
+container.empty(); // removes html structure inside container 
+
+container.html(); // returns current html structure
+container.html('<div></div>'); //changes container's html to passed one
+
+container.text('new text'); // changes text of container to passed 
